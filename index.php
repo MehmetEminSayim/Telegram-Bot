d086afb2ddeeda915df29869ceed8ffa032e96f6
Mehmet Emin Sayım <55194309+MehmetEminSayim@users.noreply.github.com>
2021-05-11 00:54:57 +0300

    telegram bot start

--- index.php
+++ index.php
@@ -1,19 +1,24 @@
-<?php 
-
+<?php
 class TelegramBot {
-    const API_URL = "ttps://api.telegram.org/bot";
+    const API_URL = "https://api.telegram.org/bot";
     public function setToken($token){
         $this->token = $token;
+    }
+
+    public function setWebhook($url){
+        return $this->request('setWebhook',[
+             'url' => $url
+        ]);
     }
 
     public function request($method,$data){
         $ch = curl_init();
         $url = self::API_URL . $this->token. '/' .$method;
 
-        curl_setopt($ch, CURLOPT_URL, url);
+        curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
         
         $headers = array();
         $headers[] = 'Content-Type: application/x-www-form-urlencoded';
@@ -24,9 +29,12 @@ class TelegramBot {
             echo 'Error:' . curl_error($ch);
         }
         curl_close($ch);
+        return $result;
     }
 
 }
 
 $telegram = new TelegramBot();
 $telegram->setToken("1757394693:AAFpXxU5XfwQqmMd6kiw20d20kqCHdq9BHY");
+
+echo $telegram->setWebhook("https://metge.requestcatcher.com");
